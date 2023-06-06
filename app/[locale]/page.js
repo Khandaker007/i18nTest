1d554ffba008cc5a0e1ab6c9b9937c9e98388555
Khandaker Nabiul <na.ninad60@gmail.com>
2023-06-06 11:49:47 +0600

    Environment setup for i18n

---
+++ app/[locale]/page.js
@@ -0,0 +1,19 @@
+import Image from 'next/image';
+import styles from './page.module.css';
+
+import {useTranslations} from 'next-intl';
+import Link from 'next-intl/link';
+
+export default function Home() {
+  const t = useTranslations('Index');
+
+  return (
+    <>
+      <div className={styles.linkContainer}>
+        <Link href="/" locale="en">Switch to English</Link>
+        <Link href="/" locale="de">Switch to German</Link>
+      </div>
+      <h1>{t('title')}</h1>
+    </>
+  )
+};
